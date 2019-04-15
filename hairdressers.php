f2a24d2a1263f9f12bd9bd6defa553069351a661
unknown <jeremy7an@gmail.com>
2019-04-15 17:35:56 +0800

    Adding php files, changing format for about page

---
+++ hairdressers.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang ="en" data-ng-app="">
+<head>
+<title>Smile and Style</title>
+<meta name="viewport" content="width=device-width, initialscale=1.0"/>
+<!-- Bootstrap -->
+<link href="framework/css/bootstrap.min.css" rel="stylesheet" />
+<link href="framework/css/styles.css" rel="stylesheet" />
+<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+<link rel="stylesheet" type="text/css" href="outsource/css/ionicons.min.css"/>
+<link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet"/>
+<link rel="stylesheet" type="text/css" href="outsource/css/ionicons.min.css"/>
+    
+ <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+ <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+ <!--[if lt IE 9]>
+ <script src="framework/js/html5shiv.js"></script>
+ <script src="framework/js/respond.min.js"></script>
+ <![endif]-->
+    
+</head>
+<body>
+    <div class="row">
+            
+        <div class="nav col-md-12 col-sm-12 col-xs-12">
+            <ul>
+                <div class="home_icon">
+                    <li><a href="hairsalontemplate.php">Home</a></li>
+                </div>
+                <li><a href="sign_in.php">Admin Sign In</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="hairdressers.php">Our hairdressers</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="timeslot.php">View Hairdressers' Schedule</a></li>
+            </ul>
+            <hr/>
+        </div>
+    </div>
+   
+   <div class="container">
+        <h2>Our Hairdressers</h2>
+       <p>Striving to serve you better </p>
+    </div>
+       
+       
+        <div class="container">
+   <div class="row">
+      <div class="center row">
+
+          <div class="col-md-4 col-sm-4 col-xs-4">
+                  <img alt="profile pic" src="framework/resources/images/default_profile.png"/>
+          </div>
+           <div class="col-md-4 col-sm-4 col-xs-4">
+                   <img alt="profile pic" src="framework/resources/images/default_profile.png"/>
+           </div>
+           <div class="col-md-4 col-sm-4 col-xs-4">
+                  <img alt="profile pic" src="framework/resources/images/default_profile.png"/>
+          </div>
+
+      </div>
+     
+          <div class="row">
+      
+          <div class="col-md-4 col-sm-4 col-xs-4">
+              <h5>Sum Ting Wong</h5>
+              <h6>Lead Hairstylist</h6>
+              <p>Been serving in the team for the years that Smile &amp; Style has been open. Is a familiar with customers who regularly come back.</p>
+          </div>
+           <div class="col-md-4 col-sm-4 col-xs-4">
+               <h5>Noh Yu Mor</h5>
+               <h6>Senior Hairstylist</h6>
+               <p>New to the team, have been in a the hairstyling business for a few year. Here to know you more.</p>
+           </div>
+           <div class="col-md-4 col-sm-4 col-xs-2">
+               <h5>Freddy Jupiter</h5>
+               <h6>Senior Hairstylist</h6>
+               <p>Several years of styling experience.</p>
+           </div>
+
+      </div>   
+   </div>
+    <div class="row">
+      <div class="center row">
+
+          <div class="col-md-4 col-sm-4 col-xs-4 offset-sm-4">
+                  <img alt="profile pic" src="framework/resources/images/default_profile.png"/>
+          </div>
+           <div class="col-md-4 col-sm-4 col-xs-4">
+                   <img alt="profile pic" src="framework/resources/images/default_profile.png"/>
+           </div>
+           <div class="col-md-4 col-sm-4 col-xs-4">
+                  <img alt="profile pic" src="framework/resources/images/default_profile.png"/>
+          </div>
+
+      </div>
+      
+      <div class="row">
+
+          <div class="col-md-4 col-sm-4 col-xs-4">
+              <h5>Tom Jerry</h5>
+              <h6>Managing Director</h6>
+               <p>Founder of Styles and Smiles</p>
+          </div>
+           <div class="col-md-4 col-sm-4 col-xs-4">
+               <h5>Ek Zack Lee</h5>
+               <h6>Trainee</h6>
+               <p>Blank for now</p>
+           </div>
+           <div class="col-md-4 col-sm-4 col-xs-2">
+               <h5>Tan Os</h5>
+               <h6>Trainee</h6>
+               <p>Goal to be here in the long run. Will be with you in a snap</p>
+           </div>
+
+      </div>   
+   </div>
+          
+      </div>
+      
+   
+    
+   <div class="row">
+        <div class="footer col-md-6 col-sm-6 col-xs-6"> 
+            <ul>
+                <li><a href="timeslot.php">View Hairdressers' Schedule</a></li>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="hairdressers.php">Our hairdressers</a></li>
+                <li><a href="about.php">About Us</a></li>  
+            </ul>
+        </div>
+        <div class="footer col-md-6 col-sm-6 col-xs-6"> 
+            <ul>
+                <li>Be our <b>V.I.P</b></li>
+                <li>Jalan Sotong 1, Taman Monyet</li>
+                <li>10000 Kuching, Sarawak</li>
+                <li><b>Follow us on social media</b></li>
+                <br/>
+                <div class="footer_container">
+                    <div class="footer_left">
+                        <a href="https://www.facebook.com"><i class="ion-social-facebook"></i></a>
+                    </div>
+                    
+                    <div class="footer_center">
+                        <a href="https://www.instagram.com"><i class="ion-social-instagram-outline"></i></a>
+                    </div>
+                    
+                    <div class="footer_right">
+                        <a href="https://www.twitter.com"><i class="ion-social-twitter"></i></a>
+                    </div>
+                </div> 
+            </ul>
+        </div>
+        
+    </div>
+    
+   
+   
+ 
+ <!-- jQuery – required for Bootstrap's JavaScript plugins) -->
+ <script src="framework/js/jquery.min.js"></script>
+<!--  All Bootstrap plug-ins file -->
+ <script src="framework/js/bootstrap.min.js"></script>
+ <!-- Basic AngularJS -->
+    <script src="framework/js/angular.min.js"></script>
+ </body>
+</html>
+
---
+++ products.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang ="en" data-ng-app="">
+<head>
+<title>Smile and Style</title>
+<meta name="viewport" content="width=device-width, initialscale=1.0"/>
+<!-- Bootstrap -->
+<link href="framework/css/bootstrap.min.css" rel="stylesheet" />
+<link href="framework/css/styles.css" rel="stylesheet" />
+<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+<link rel="stylesheet" type="text/css" href="outsource/css/ionicons.min.css"/>
+<link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet"/>
+<link rel="stylesheet" type="text/css" href="outsource/css/ionicons.min.css"/>
+    
+ <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+ <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+ <!--[if lt IE 9]>
+ <script src="framework/js/html5shiv.js"></script>
+ <script src="framework/js/respond.min.js"></script>
+ <![endif]-->
+    
+</head>
+<body>
+    <div class="row">
+            
+        <div class="nav col-md-12 col-sm-12 col-xs-12">
+            <ul>
+                <div class="home_icon">
+                    <li><a href="hairsalontemplate.php">Home</a></li>
+                </div>
+                <li><a href="sign_in.php">Admin Sign In</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="hairdressers.php">Our hairdressers</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="timeslot.php">View Hairdressers' Schedule</a></li>
+            </ul>
+            <hr/>
+        </div>
+    </div>
+    <div class="space"></div>
+    <div class="container">
+        <h2>Products</h2>
+   
+   <div class="row">
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product" src="framework/resources/products/product1.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>Art Hairspray(420ml)</h5>
+                    <h5><strong>RM 12.00</strong></h5>
+<!--
+                    <ul>
+                        <li>Professional Hair Spray</li>
+                        <li>for everyday use</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product" src="framework/resources/products/product2.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>L'Oreal Professional Hair SPA Detoxifying Shampoo(1500ml)</h5>
+                    <h5>RM65.00</h5>
+<!--
+                    <ul>
+                        <li>Enriched with tea tree oil</li>
+                        <li>Cleases and frees your scalp from impurities</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product" src="framework/resources/products/product3.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>Notel Spiky Clap (100ml)</h5>
+                    <h5>RM25.00</h5>
+<!--
+                    <ul>
+                        <li>Great for matt and choppy style</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product" src="framework/resources/products/product4.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>Shiseido Professional THC Aqua Intensive Treatment 1 Airy Feel Conditioner (1000g)</h5>
+                    <h5>RM300.00</h5>
+<!--
+                    <ul>
+                        <li>Prevents dry and damage hair</li>
+                        <li>Makes hair deepy hydrated</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product"  src="framework/resources/products/product5.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>Kerastase Elixir Ultime (100ml)</h5>
+                    <h5>RM120.00</h5>
+<!--
+                    <ul>
+                        <li>Versatile, award winning &amp; beautifying hair oil rich in four precious oils Maize, Argan, Camellia &amp; Pracaxi oils</li>
+                        <li>Provides instant moisture without overloading hair Leaves hair soft, smooth, shiny &amp; protected</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product" src="framework/resources/products/product6.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>Shiseido Professional THC Adenovital Advanced Scalp Essence 180ml</h5>
+                    <h5><strong>RM230.00</strong></h5>
+<!--
+                    <ul>
+                        <li>Description</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product" src="framework/resources/products/product7.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>MNM Aromatic Hairspray (420ml)</h5>
+                    <h5><strong>RM12.00</strong></h5>
+<!--
+                    <ul>
+                        <li>Description</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product" src="framework/resources/products/product8.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>Schwarzkopf Osis+ Dust It Mattifying Powder 10g</h5>
+                    <h5><strong>RM30.00</strong></h5>
+<!--
+                    <ul>
+                        <li>Description</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product" src="framework/resources/products/product9.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>Ketastase Resistance Ciment Thermique (150ml)</h5>
+                    <h5><strong>RM90.00</strong></h5>
+<!--
+                    <ul>
+                        <li>Description</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product" src="framework/resources/products/product10.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>Elegance Plus Matte Hair Wax (140g)</h5>
+                    <h5><strong>RM45.00</strong></h5>
+<!--
+                    <ul>
+                        <li>Description</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product" src="framework/resources/products/product11.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>Arimino Spice Water Shining Straight</h5>
+                    <h5><strong>RM90.00</strong></h5>
+<!--
+                    <ul>
+                        <li>Description</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <img class="product" alt="product" src="framework/resources/products/product12.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h5>L'Oreal Professionnel Serioxyl Stemoxydine 5% Neohesperidin Denser Hair Treatment (90ml)</h5>
+                    <h5><strong>RM150.00</strong></h5>
+<!--
+                    <ul>
+                        <li>Description</li>
+                    </ul>
+-->
+                </div>
+            </div>
+        </div>
+    </div>
+   
+                </div>
+            
+        
+    
+    
+    
+       
+
+
+    <div class="row">
+        <div class="footer col-md-6 col-sm-6 col-xs-6"> 
+            <ul>
+                <li><a href="timeslot.php">View Hairdressers' Schedule</a></li>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="hairdressers.php">Our hairdressers</a></li>
+                <li><a href="about.php">About Us</a></li>  
+            </ul>
+        </div>
+        <div class="footer col-md-6 col-sm-6 col-xs-6"> 
+            <ul>
+                <li>Be our <b>V.I.P</b></li>
+                <li>Jalan Sotong 1, Taman Monyet</li>
+                <li>10000 Kuching, Sarawak</li>
+                <li><b>Follow us on social media</b></li>
+                <br/>
+                <div class="footer_container">
+                    <div class="footer_left">
+                        <a href="https://www.facebook.com"><i class="ion-social-facebook"></i></a>
+                    </div>
+                    
+                    <div class="footer_center">
+                        <a href="https://www.instagram.com"><i class="ion-social-instagram-outline"></i></a>
+                    </div>
+                    
+                    <div class="footer_right">
+                        <a href="https://www.twitter.com"><i class="ion-social-twitter"></i></a>
+                    </div>
+                </div> 
+            </ul>
+        </div>
+        
+    </div>
+    
+   
+   
+ 
+ <!-- jQuery – required for Bootstrap's JavaScript plugins) -->
+ <script src="framework/js/jquery.min.js"></script>
+<!--  All Bootstrap plug-ins file -->
+ <script src="framework/js/bootstrap.min.js"></script>
+ <!-- Basic AngularJS -->
+    <script src="framework/js/angular.min.js"></script>
+ </body>
+</html>
+
