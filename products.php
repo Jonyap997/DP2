7e2819b54aa1b69b48fd29d6e092af39914d970e
jeryxem <jeryxem@gmail.com>
2019-04-17 23:41:39 +0800

    media query fix for products

--- products.php
+++ products.php
@@ -2,7 +2,7 @@
 <html lang ="en" data-ng-app="">
 <head>
 <title>Smile and Style</title>
-<meta name="viewport" content="width=device-width, initialscale=1.0"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <!-- Bootstrap -->
 <link href="framework/css/bootstrap.min.css" rel="stylesheet" />
 <link href="framework/css/styles.css" rel="stylesheet" />
@@ -10,6 +10,9 @@
 <link rel="stylesheet" type="text/css" href="outsource/css/ionicons.min.css"/>
 <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet"/>
 <link rel="stylesheet" type="text/css" href="outsource/css/ionicons.min.css"/>
+   
+<!-- Media Queries-->
+<link href="framework/css/mediaqueries.css" rel="stylesheet" />
     
  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -25,7 +28,7 @@
         <div class="nav col-md-12 col-sm-12 col-xs-12">
             <ul>
                 <div class="home_icon">
-                    <li><a href="hairsalontemplate.php">Home</a></li>
+                    <li><a href="index.php">Home</a></li>
                 </div>
                 <li><a href="loginPage.php">Admin Sign In</a></li>
                 <li><a href="about.php">About Us</a></li>
@@ -38,173 +41,173 @@
         </div>
     </div>
     <div class="space"></div>
-    <div class="container">
-        <h2>Products</h2>
+    <div class="containerProduct">
+            <h2>Products</h2>
    
-   <div class="row">
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product" src="framework/resources/products/product1.jpg" />
+       <div class="row">
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product" src="framework/resources/products/product1.jpg" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>Art Hairspray(420ml)</p>
+                        <p><strong>RM 12.00</strong></p>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>Art Hairspray(420ml)</h5>
-                    <h5><strong>RM 12.00</strong></h5>
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product" src="framework/resources/products/product2.jpg" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>L'Oreal Professional Hair SPA Detoxifying Shampoo(1500ml)</p>
+                        <p><strong>RM65.00</strong></p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product" src="framework/resources/products/product2.jpg" />
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product" src="framework/resources/products/product3.jpg" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>Notel Spiky Clap (100ml)</p>
+                        <p><strong>RM25.00</strong></p>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>L'Oreal Professional Hair SPA Detoxifying Shampoo(1500ml)</h5>
-                    <h5><strong>RM65.00</strong></h5>
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product" src="framework/resources/products/product4.jpg" />
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product" src="framework/resources/products/product3.jpg" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>Notel Spiky Clap (100ml)</h5>
-                    <h5><strong>RM25.00</strong></h5>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product" src="framework/resources/products/product4.jpg" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>Shiseido Professional THC Aqua Intensive Treatment 1 Airy Feel Conditioner (1000g)</h5>
-                    <h5><strong>RM300.00</strong></h5>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product"  src="framework/resources/products/product5.jpg" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>Kerastase Elixir Ultime (100ml)</h5>
-                    <h5><strong>RM120.00</strong></h5>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>Shiseido Professional THC Aqua Intensive Treatment 1 Airy Feel Conditioner (1000g)</p>
+                        <p><strong>RM300.00</strong></p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product" src="framework/resources/products/product6.jpg" />
+        <div class="row">
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product"  src="framework/resources/products/product5.jpg" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>Kerastase Elixir Ultime (100ml)</p>
+                        <p><strong>RM120.00</strong></p>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>Shiseido Professional THC Adenovital Advanced Scalp Essence 180ml</h5>
-                    <h5><strong>RM230.00</strong></h5>
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product" src="framework/resources/products/product6.jpg" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>Shiseido Professional THC Adenovital Advanced Scalp Essence 180ml</p>
+                        <p><strong>RM230.00</strong></p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product" src="framework/resources/products/product7.jpg" />
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product" src="framework/resources/products/product7.jpg" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>MNM Aromatic Hairspray (420ml)</p>
+                        <p><strong>RM12.00</strong></p>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>MNM Aromatic Hairspray (420ml)</h5>
-                    <h5><strong>RM12.00</strong></h5>
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product" src="framework/resources/products/product8.jpg" />
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product" src="framework/resources/products/product8.jpg" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>Schwarzkopf Osis+ Dust It Mattifying Powder 10g</h5>
-                    <h5><strong>RM30.00</strong></h5>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product" src="framework/resources/products/product9.jpg" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>Ketastase Resistance Ciment Thermique (150ml)</h5>
-                    <h5><strong>RM90.00</strong></h5>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product" src="framework/resources/products/product10.jpg" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>Elegance Plus Matte Hair Wax (140g)</h5>
-                    <h5><strong>RM45.00</strong></h5>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>Schwarzkopf Osis+ Dust It Mattifying Powder 10g</p>
+                        <p><strong>RM30.00</strong></p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product" src="framework/resources/products/product11.jpg" />
+        <div class="row">
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product" src="framework/resources/products/product9.jpg" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>Ketastase Resistance Ciment Thermique (150ml)</p>
+                        <p><strong>RM90.00</strong></p>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>Arimino Spice Water Shining Straight</h5>
-                    <h5><strong>RM90.00</strong></h5>
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product" src="framework/resources/products/product10.jpg" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>Elegance Plus Matte Hair Wax (140g)</p>
+                        <p><strong>RM45.00</strong></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product" src="framework/resources/products/product11.jpg" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>Arimino Spice Water Shining Straight</p>
+                        <p><strong>RM90.00</strong></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <img class="product" alt="product" src="framework/resources/products/product12.jpg" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <p>L'Oreal Professionnel Serioxyl Stemoxydine 5% Neohesperidin Denser Hair Treatment (90ml)</p>
+                        <p><strong>RM150.00</strong></p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-3 col-sm-3 col-xs-3">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <img class="product" alt="product" src="framework/resources/products/product12.jpg" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5>L'Oreal Professionnel Serioxyl Stemoxydine 5% Neohesperidin Denser Hair Treatment (90ml)</h5>
-                    <h5><strong>RM150.00</strong></h5>
-                </div>
-            </div>
-        </div>
-    </div>
    
-                </div>
+</div>
             
         
     
@@ -213,8 +216,8 @@
        
 
 
-    <div class="row">
-        <div class="footer col-md-6 col-sm-6 col-xs-6"> 
+    <div class="row footer">
+        <div class="col-md-6 col-sm-6 col-xs-6"> 
             <ul>
                 <li><a href="timeslot.php">View Hairdressers' Schedule</a></li>
                 <li><a href="products.php">Products</a></li>
@@ -223,7 +226,7 @@
                 <li><a href="about.php">About Us</a></li>  
             </ul>
         </div>
-        <div class="footer col-md-6 col-sm-6 col-xs-6"> 
+        <div class="col-md-6 col-sm-6 col-xs-6"> 
             <ul>
                 <li>Be our <b>V.I.P</b></li>
                 <li>Jalan Sotong 1, Taman Monyet</li>
