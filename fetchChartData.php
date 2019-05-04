09c770a0fc9a88e4f0f47cb77ed959df5f93998b
Jonyap997 <jonyap997@gmail.com>
2019-05-04 20:46:33 +0800

    Update fetchChartData.php

--- fetchChartData.php
+++ fetchChartData.php
@@ -81,4 +81,31 @@
         header("Location: fetchChartData.php");
         
     }
+
+    // get sales data for sales report chart
+    if (isset($_POST['sales_report'])) {
+        
+        $result = [];
+        
+        //Get Values from form
+        $year = $_POST['report_year_select'];
+        
+        //To prevent sql injection
+        $year = stripcslashes($year);
+
+        $year = mysqli_real_escape_string($connection, $year);
+        
+        //query to get data from the table
+        for ($i = 0; $i < 12; i++)
+        {
+            $month = $i + 1;
+            $hairdresser_id = 1001 + $i;
+            $result[i] = mysqli_query($connection, "SELECT SUM(total) FROM customerPurchaseHistory WHERE $year = YEAR(datePurchased) AND $month = MONTH(datePurchased)")
+            or die("Error".mysqli_error($connection));
+        }
+        
+        $_SESSION['msg'] = "Data Updated";
+        header("Location: fetchChartData.php");
+        
+    }
     ?>
